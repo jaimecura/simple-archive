0f64a4cdf485c75d6c076553f714024f87234ae7
Jaime Cura <jaimecura@gmail.com>
2024-07-17 20:44:29 +0200

    avoid unsafe when not needed and some refactoring

--- src/writer.rs
+++ src/writer.rs
@@ -1,10 +1,9 @@
-use crate::prelude::*;
+use crate::{prelude::*, Metadata};
 
 use std::{
     ffi::CString,
-    fs::{File, Metadata as FSMeta},
+    fs::File,
     io::{Read, Write},
-    os::unix::fs::MetadataExt,
     ptr::null_mut,
 };
 
@@ -13,79 +12,38 @@ use libc::c_void;
 use crate::{
     carchive::{
         self, archive, archive_entry_free, archive_entry_new, archive_entry_set_atime,
-        archive_entry_set_ctime, archive_entry_set_mode, archive_entry_set_mtime,
-        archive_entry_set_pathname, archive_entry_set_perm, archive_entry_set_size,
-        archive_write_data, archive_write_free, archive_write_header, mode_t, AE_IFREG,
+        archive_entry_set_ctime, archive_entry_set_gid, archive_entry_set_mode,
+        archive_entry_set_mtime, archive_entry_set_pathname, archive_entry_set_perm,
+        archive_entry_set_size, archive_entry_set_uid, archive_write_data, archive_write_free,
+        archive_write_header,
     },
-    error::archive_result,
-    AE_IFDIR, AE_IFLNK, ARCHIVE_FILTER_BZIP2, ARCHIVE_FILTER_GZIP, ARCHIVE_FILTER_LRZIP,
-    ARCHIVE_FILTER_LZ4, ARCHIVE_FILTER_LZIP, ARCHIVE_FILTER_LZMA, ARCHIVE_FILTER_LZOP,
-    ARCHIVE_FILTER_NONE, ARCHIVE_FILTER_XZ, ARCHIVE_FILTER_ZSTD, ARCHIVE_FORMAT_7ZIP,
-    ARCHIVE_FORMAT_TAR, ARCHIVE_FORMAT_XAR, ARCHIVE_FORMAT_ZIP,
+    ARCHIVE_FILTER_BZIP2, ARCHIVE_FILTER_GZIP, ARCHIVE_FILTER_LRZIP, ARCHIVE_FILTER_LZ4,
+    ARCHIVE_FILTER_LZIP, ARCHIVE_FILTER_LZMA, ARCHIVE_FILTER_LZOP, ARCHIVE_FILTER_NONE,
+    ARCHIVE_FILTER_XZ, ARCHIVE_FILTER_ZSTD, ARCHIVE_FORMAT_7ZIP, ARCHIVE_FORMAT_TAR,
+    ARCHIVE_FORMAT_XAR, ARCHIVE_FORMAT_ZIP,
 };
 use std::os::raw::c_int;
 
 const BUFFER_SIZE: usize = 16384;
 
-pub struct ArchiveWriter<R: Write> {
+pub struct ArchiveWriter<W: Write> {
     archive_writer: *mut archive,
-    fileref: Box<FileWriter<R>>,
+    fileref: Box<FileWriter<W>>,
     file_format: c_int,
     file_filter: c_int,
 }
 
-struct FileWriter<R: Write> {
-    obj: R,
+struct FileWriter<W: Write> {
+    obj: W,
 }
 
-pub struct Metadata {
-    size: i64,
-    nodetype: u32,
-    perm: mode_t,
-    ctime: i64,
-    ctime_nano: i64,
-    atime: i64,
-    atime_nano: i64,
-    mtime: i64,
-    mtime_nano: i64,
-}
-
-fn into_nodetype(source: &FSMeta) -> u32 {
-    if source.is_dir() {
-        AE_IFDIR
-    } else if source.is_file() {
-        AE_IFREG
-    } else if source.is_symlink() {
-        AE_IFLNK
-    } else {
-        0
-    }
-}
-
-impl From<FSMeta> for Metadata {
-    fn from(meta: FSMeta) -> Self {
-        Metadata {
-            size: meta.size() as i64, // Iḿ assuming anything above 2**63 is way to much for
-            // a single file
-            nodetype: into_nodetype(&meta),
-            perm: meta.mode(),
-            ctime: meta.ctime(),
-            ctime_nano: meta.ctime_nsec(),
-            atime: meta.atime(),
-            atime_nano: meta.atime_nsec(),
-            mtime: meta.mtime(),
-            mtime_nano: meta.mtime_nsec(),
-        }
-    }
-}
-
-unsafe extern "C" fn archivewriter_writer<R: Write>(
+unsafe extern "C" fn archivewriter_write<W: Write>(
     archive: *mut carchive::archive,
     client_data: *mut c_void,
     buffer: *const c_void,
     size: usize,
 ) -> carchive::la_ssize_t {
-    let writer = (client_data as *mut FileWriter<R>).as_mut().unwrap();
+    let writer = (client_data as *mut FileWriter<W>).as_mut().unwrap();
     let writable = std::slice::from_raw_parts(buffer as *const u8, size);
 
     match writer.obj.write(writable) {
@@ -102,10 +60,10 @@ unsafe extern "C" fn archivewriter_writer<R: Write>(
     }
 }
 
-impl<R: Write> ArchiveWriter<R> {
-    pub fn new(dest: R) -> Result<ArchiveWriter<R>>
+impl<W: Write> ArchiveWriter<W> {
+    pub fn new(dest: W) -> Result<ArchiveWriter<W>>
     where
-        R: Write,
+        W: Write,
     {
         let fref = Box::new(FileWriter { obj: dest });
         unsafe {
@@ -115,10 +73,10 @@ impl<R: Write> ArchiveWriter<R> {
                 return Err(Error::NullArchive);
             }
 
-            archive_result(
-                carchive::archive_write_set_bytes_in_last_block(archive_writer, 1),
-                archive_writer,
-            )?;
+            match carchive::archive_write_set_bytes_in_last_block(archive_writer, 1) {
+                carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+                _ => return Err(Error::from(archive_writer)),
+            };
 
             Ok(ArchiveWriter {
                 archive_writer,
@@ -131,22 +89,19 @@ impl<R: Write> ArchiveWriter<R> {
 
     // Raw Archive API
     pub fn set_output_format(&mut self, format: c_int) -> Result<()> {
-        unsafe {
-            archive_result(
-                carchive::archive_write_set_format(self.archive_writer, format),
-                self.archive_writer,
-            )?;
+        match unsafe { carchive::archive_write_set_format(self.archive_writer, format) } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+            _ => return Err(Error::from(self.archive_writer)),
         }
+
         self.file_format = format;
         Ok(())
     }
 
     pub fn set_output_filter(&mut self, filter: c_int) -> Result<()> {
-        unsafe {
-            archive_result(
-                carchive::archive_write_add_filter(self.archive_writer, filter),
-                self.archive_writer,
-            )?;
+        match unsafe { carchive::archive_write_add_filter(self.archive_writer, filter) } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+            _ => return Err(Error::from(self.archive_writer)),
         }
         self.file_filter = filter;
         Ok(())
@@ -155,34 +110,34 @@ impl<R: Write> ArchiveWriter<R> {
     pub fn add_filter_option(&mut self, name: &str, value: &str) -> Result<()> {
         let n = CString::new(name.to_string()).unwrap();
         let v = CString::new(value.to_string()).unwrap();
-        unsafe {
-            archive_result(
-                carchive::archive_write_set_filter_option(
-                    self.archive_writer,
-                    null_mut(),
-                    n.as_ptr(),
-                    v.as_ptr(),
-                ),
+        match unsafe {
+            carchive::archive_write_set_filter_option(
                 self.archive_writer,
-            )?;
-        };
+                null_mut(),
+                n.as_ptr(),
+                v.as_ptr(),
+            )
+        } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+            _ => return Err(Error::from(self.archive_writer)),
+        }
         Ok(())
     }
 
     pub fn add_format_option(&mut self, name: &str, value: &str) -> Result<()> {
         let n = CString::new(name.to_string()).unwrap();
         let v = CString::new(value.to_string()).unwrap();
-        unsafe {
-            archive_result(
-                carchive::archive_write_set_format_option(
-                    self.archive_writer,
-                    null_mut(),
-                    n.as_ptr(),
-                    v.as_ptr(),
-                ),
+        match unsafe {
+            carchive::archive_write_set_format_option(
                 self.archive_writer,
-            )?;
-        };
+                null_mut(),
+                n.as_ptr(),
+                v.as_ptr(),
+            )
+        } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+            _ => return Err(Error::from(self.archive_writer)),
+        }
         Ok(())
     }
 
@@ -190,24 +145,27 @@ impl<R: Write> ArchiveWriter<R> {
         if self.file_format < 0 || self.file_filter < 0 {
             return Err(Error::IncompleteInitialization);
         }
-        unsafe {
-            archive_result(
-                carchive::archive_write_open(
-                    self.archive_writer,
-                    std::ptr::addr_of_mut!(*self.fileref) as *mut c_void,
-                    None,
-                    Some(archivewriter_writer::<R>),
-                    None,
-                ),
+        match unsafe {
+            carchive::archive_write_open(
                 self.archive_writer,
-            )?;
+                std::ptr::addr_of_mut!(*self.fileref) as *mut c_void,
+                None,
+                Some(archivewriter_write::<W>),
+                None,
+            )
+        } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+            _ => return Err(Error::from(self.archive_writer)),
         }
         Ok(())
     }
 
     // this free is not meant to called directly. Only by borrow system
     fn free(&mut self) -> Result<()> {
-        unsafe { archive_result(archive_write_free(self.archive_writer), self.archive_writer) }
+        match unsafe { archive_write_free(self.archive_writer) } {
+            carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => Ok(()),
+            _ => Err(Error::from(self.archive_writer)),
+        }
     }
 
     // this is only for output write filter.
@@ -327,20 +285,22 @@ impl<R: Write> ArchiveWriter<R> {
         unsafe {
             let entry = archive_entry_new();
 
-            archive_entry_set_size(entry, objmeta.size); // quick way to get the size?
+            archive_entry_set_size(entry, objmeta.size()); // quick way to get the size?
                                                          // archive_entry_set_perm(entry, 0o777);
                                                          // archive_entry_set_filetype(entry, AE_IFREG);
-            archive_entry_set_mode(entry, objmeta.nodetype | objmeta.perm);
-            archive_entry_set_perm(entry, objmeta.perm);
-            archive_entry_set_ctime(entry, objmeta.ctime, objmeta.ctime_nano);
-            archive_entry_set_mtime(entry, objmeta.mtime, objmeta.mtime_nano);
-            archive_entry_set_atime(entry, objmeta.atime, objmeta.atime_nano);
+            archive_entry_set_mode(entry, objmeta.nodetype() | objmeta.perm());
+            archive_entry_set_perm(entry, objmeta.perm());
+            archive_entry_set_ctime(entry, objmeta.ctime(), objmeta.ctime_nano());
+            archive_entry_set_mtime(entry, objmeta.mtime(), objmeta.mtime_nano());
+            archive_entry_set_atime(entry, objmeta.atime(), objmeta.atime_nano());
             archive_entry_set_pathname(entry, p.as_ptr());
+            archive_entry_set_uid(entry, objmeta.owner());
+            archive_entry_set_gid(entry, objmeta.group());
 
-            archive_result(
-                archive_write_header(self.archive_writer, entry),
-                self.archive_writer,
-            )?;
+            match archive_write_header(self.archive_writer, entry) {
+                carchive::ARCHIVE_OK | carchive::ARCHIVE_WARN => (),
+                _ => return Err(Error::from(self.archive_writer)),
+            }
 
             loop {
                 let readed = source.read(&mut buffer)?;
@@ -371,7 +331,7 @@ impl<R: Write> ArchiveWriter<R> {
     }
 }
 
-impl<R: Write> Drop for ArchiveWriter<R> {
+impl<W: Write> Drop for ArchiveWriter<W> {
     fn drop(&mut self) {
         drop(self.free());
     }
