852080d153f3382db2b1aa2f39f1d370fe468be2
Jaime Cura <jaimecura@gmail.com>
2024-08-12 23:54:48 +0200

    add the simplest test to check if decompression is right

--- tests/uncompress.rs
+++ tests/uncompress.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use core::panic;
+use std::{fs::File, io::Read};
 
 use simple_archive::reader::ArchiveReader;
 
@@ -10,3 +11,18 @@ fn seek_to_obj() {
     a.reader_seek_obj("random.txt").unwrap();
 }
 
+#[test]
+fn read_obj() {
+    let source = File::open("tests/fixtures/single_file.tar.bz2").unwrap();
+    let mut a = ArchiveReader::new(source).unwrap();
+    a.reader_seek_obj("test2.txt").unwrap();
+
+    let mut buf = [0u8; 1024];
+    let amount = a.read(&mut buf).unwrap();
+    let hash = sha256::digest(&buf[..amount]);
+    let disk_data = std::fs::read("tests/fixtures/test2.txt").unwrap();
+    let hash2 = sha256::digest(disk_data);
+    if hash != hash2 {
+        panic!("wrong uncompressed data")
+    }
+}
